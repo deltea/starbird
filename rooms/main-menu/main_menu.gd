d4b37e72ff4de86e5c2bcb7590bde7d6fca47e20
deltea <thcheetah777@gmail.com>
2026-03-07 09:29:08 -0800

    jesus this main menu is a mess (as always)

--- rooms/main-menu/main_menu.gd
+++ rooms/main-menu/main_menu.gd
@@ -4,14 +4,21 @@ enum MenuState { MAIN, SETTINGS }
 
 const shooting_star_scene = preload("res://scenes/particles/shooting-star/shooting_star.tscn")
 
-@onready var star: Star2D = $Star2D
-@onready var title_star: Star2D = $Title/Star
-@onready var trailing_star: Star2D = $Star2D/TrailingStar
-@onready var option_selector: NinePatchRect = $OptionSelector
+@onready var big_star: Star2D = $BigStar
+@onready var trailing_star: Star2D = $BigStar/TrailingStar
 @onready var selector_ping_timer: Timer = $SelectorPingTimer
 @onready var shooting_star_timer: Timer = $ShootingStarTimer
-@onready var options: VBoxContainer = $Options
 @onready var camera_target: Node2D = $CameraTarget
+
+# main menu stuff
+@onready var title_star: Star2D = $Main/Title/Star
+@onready var option_selector: NinePatchRect = $Main/OptionSelector
+@onready var options: VBoxContainer = $Main/Options
+
+# settings stuff
+@onready var settings_option_selector: NinePatchRect = $Settings/OptionSelector
+@onready var settings_options: VBoxContainer = $Settings/Options
+@onready var settings_option_stars: Node2D = $Settings/OptionStars
 
 @onready var star_rot_dynamics: DynamicsSolver = Dynamics.create_dynamics(4.0, 0.4, 2.0)
 
@@ -20,17 +27,15 @@ var select_index = 0
 var selector_target_y = 0.0
 var original_selector_width = 0.0
 var state = MenuState.MAIN
-var original_star_y
 
 func _ready() -> void:
 	selector_target_y = option_selector.position.y
 	original_selector_width = option_selector.size.x
-	original_star_y = star.position.y
 
 func _process(dt: float) -> void:
-	star.rotation_degrees = star_rot_dynamics.update(target_rot)
-	trailing_star.rotation_degrees = lerp(trailing_star.rotation_degrees, star.rotation_degrees + 12.0, 10.0 * dt)
-	trailing_star.position = star.position
+	big_star.rotation_degrees = star_rot_dynamics.update(target_rot)
+	trailing_star.rotation_degrees = lerp(trailing_star.rotation_degrees, big_star.rotation_degrees + 12.0, 10.0 * dt)
+	trailing_star.position = big_star.position
 	title_star.rotation_degrees += 160.0 * dt
 
 	match state:
@@ -58,7 +63,6 @@ func change_state(new_state: MenuState):
 func main_state(dt: float):
 	option_selector.position.y = lerp(option_selector.position.y, selector_target_y + select_index * 25.0, 25.0 * dt)
 	option_selector.scale.x = lerp(option_selector.scale.x, 1.0, 10.0 * dt)
-	star.position.y = lerp(star.position.y, original_star_y, 15.0 * dt)
 
 	if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("down"):
 		change_index(1)
@@ -66,8 +70,6 @@ func main_state(dt: float):
 		change_index(-1)
 
 func settings_state(dt: float):
-	star.position.y = lerp(star.position.y, original_star_y - 36, 15.0 * dt)
-
 	if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("down"):
 		change_index(1, -1)
 	if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("up"):

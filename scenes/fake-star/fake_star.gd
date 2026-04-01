d08fbe302f20155042ac68b9377ca27d4ca71059
deltea <thcheetah777@gmail.com>
2026-03-31 19:42:36 -0700

    starry sky!!!

---
+++ scenes/fake-star/fake_star.gd
@@ -0,0 +1,33 @@
+class_name FakeStar extends Node2D
+
+@onready var particles: CPUParticles2D = $Particles
+
+var target_x = 0.0
+var speed = 0.0
+var rotation_dir = 1.0
+var rotation_gain = 0.0
+var rotation_speed = 60.0
+var speed_gain = 0.0
+
+func _ready() -> void:
+	scale = Vector2.ZERO
+	rotation_dir = 1 if randf() > 0.5 else -1
+	rotation_gain = randf_range(10, 100)
+	particles.emitting = false
+	speed = randf_range(30.0, 80.0)
+	speed_gain = randf_range(50.0, 100.0)
+	target_x = position.x + randf_range(-150.0, 150.0)
+
+	await Clock.wait(1.0)
+	particles.emitting = true
+
+func _process(dt: float) -> void:
+	rotation_speed += rotation_gain * dt
+	rotation_degrees += rotation_dir * rotation_speed * dt
+	position.x = lerp(position.x, target_x, 0.5 * dt)
+	scale = scale.lerp(Vector2.ONE * randf_range(1, 2), 2.0 * dt)
+	speed += speed_gain * dt
+	position.y -= speed * dt
+
+func _on_kill_timer_timeout():
+	queue_free()
--- scripts/the_peak.gd
+++ scripts/the_peak.gd
@@ -1,11 +1,13 @@
 class_name ThePeak extends Level
 
 const shooting_star_scene = preload("res://scenes/particles/shooting-star/shooting_star.tscn")
+const fake_star_scene = preload("res://scenes/fake-star/fake_star.tscn")
 
 @export var target_final_y = -1674.0
 
 @onready var final_camera_target: Node2D = $FinalCameraTarget
 @onready var shooting_star_timer: Timer = $ShootingStarTimer
+@onready var give_star_timer: Timer = $GiveStarTimer
 
 func _on_win_area_body_entered(body: Node2D) -> void:
 	if not body is Player: return
@@ -24,6 +26,12 @@ func _on_win_area_body_entered(body: Node2D) -> void:
 		SaveManager.save_game()
 
 	await Clock.wait(1.2)
+
+	# start giving stars
+	give_star_timer.start()
+
+	await Clock.wait(5.0)
+
 	camera.position_smoothing_speed = 2.0
 	final_camera_target.position.y = target_final_y
 
@@ -33,3 +41,10 @@ func _on_shooting_star_timer_timeout() -> void:
 	shooting_star.dir = Vector2(1, 1).rotated(randf_range(-PI/4, PI/4)).normalized()
 	add_child(shooting_star)
 	shooting_star_timer.start(randf_range(1.0, 2.0))
+
+func _on_give_star_timer_timeout():
+	# spawn a fake star
+	var fake_star = fake_star_scene.instantiate() as FakeStar
+	fake_star.position = player.position
+	add_child(fake_star)
+	give_star_timer.wait_time = randf_range(0.8, 1.1)
